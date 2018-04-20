510db3a9c52078f5ae9a13c8b0b00593cfe46bbc
Alexander Kostarev <UniverAlex@ya.ru>
2018-04-20 10:01:21 +0300

    if applied this will remove unused imports

--- src/main/java/ru/univeralex/web/servlet/SignUpServlet.java
+++ src/main/java/ru/univeralex/web/servlet/SignUpServlet.java
@@ -3,12 +3,10 @@ package ru.univeralex.web.servlet;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.univeralex.web.config.spring.AppConfig;
 import ru.univeralex.web.dao.impl.UserDaoJdbcImpl;
 import ru.univeralex.web.model.User;
 import ru.univeralex.web.provider.api.DataSourceProvider;
-import ru.univeralex.web.provider.impl.DataSourceProviderImpl;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,10 +15,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * @author - Alexander Kostarev
@@ -46,7 +42,7 @@ public class SignUpServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String name = req.getParameter("name");
         String password = req.getParameter("password");
         userDao.save(new User(name, BCrypt.hashpw(password, BCrypt.gensalt())));
